4a877452bc8d4fcf1e7a4feabb2e684ff2038acc
chase-stevens <chasestevenspersonal@gmail.com>
2018-10-17 11:05:17 -0700

    changed vector image to icon, reformatted forecast and code

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 
 // returns location of user and runs getWeather upon success
+// it honestly looks like everything has to run based off of the getCurrentPosition function
 function getLocation(){
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(getWeather, locationError, {timeout: 10000});
+    navigator.geolocation.getCurrentPosition(displayWeather, locationError, {timeout: 10000});
   } else {
     document.getElementById("demo").innerHTML = "Geolocation is not supported on this browser.";
   }
@@ -13,16 +14,40 @@ function locationError() {
   document.getElementById("title").innerHTML = "the geolocation is not working at the moment."
 }
 
-// processes weather data and updates site
-// TODO: refactor into separate functions
-function getWeather(position) {
-  var request = new XMLHttpRequest();
+function displayWeather(position) {
+  let coords = LatLonCoords(position);
+  console.log(coords);
+  let todayWeather = getWeatherData(coords.lat, coords.lon);
+}
 
-  // retrieving data from open api
+function LatLonCoords(position) {
   let lat = Math.round(position.coords.latitude * 100) / 100;
   let lon = Math.round(position.coords.longitude * 100) / 100;
+  let coords = { "lat": lat, "lon": lon };
+  return coords;
+}
+
+function getWeatherData(lat, lon) {
+  var request = new XMLHttpRequest();
 
   request.open('GET', `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&APPID=cf6da6d6f0df9e748deb7ac9a6b0b174`, true);
+  request.send();
+  request.onreadystatechange = processRequest;
+
+  function processRequest(e){
+    if (request.readyState === 4 && request.status === 200) {
+      var response = JSON.parse(request.responseText);
+      renderMainData(response);
+    }
+    else {
+      console.log("error with weather api");
+    }
+  }
+}
+
+/*
+function getForecastData(lat, lon) {
+  request.open('GET', `https://api.openweathermap.org/data/2.5/forecast?lat=${position.lat}&lon=${position.lon}&units=imperial&APPID=cf6da6d6f0df9e748deb7ac9a6b0b174`, true);
   request.send();
 
   request.onreadystatechange = processRequest;
@@ -32,45 +57,160 @@ function getWeather(position) {
       var response = JSON.parse(request.responseText);
 
       // displays weather icon
-      var iconImage = document.getElementById("image");
+      var iconImage = document.getElementById("weather-icon");
+      var icon = response.weather[0].id;
+    }
+  }
+}
+*/
+
+function renderMainData(apiResponse) {
+  var iconImage = document.getElementById("weather-icon");
+  var icon = apiResponse.weather[0].id;
+  // weather icon and background gradient are both dependent on icon value
+  // switch below determines the main icon and background gradient
+  switch (true) {
+
+    case (icon < 300): // thunderstorm
+      iconImage.classList.add('wi-thunderstorm');
+      document.body.style.backgroundColor = 'rgb(30,117,131)';
+      document.body.style.backgroundImage = 'linear-gradient(135deg, rgba(30,117,131,1) 0%, rgba(47,73,78,1) 68%, rgba(10,15,15,1) 100%)';
+      break;
+
+    case (icon < 400): // drizzle
+      iconImage.classList.add('wi-sprinkle');
+      document.body.style.backgroundImage = 'linear-gradient(90deg, #00d2ff 0%, #3a47d5 100%)';
+      break;
+
+    case (icon < 600): // rain
+      iconImage.classList.add('wi-rain');
+      document.body.style.backgroundImage = 'linear-gradient(90deg, #4b6cb7 0%, #182848 100%)';
+      break;
+
+    case (icon < 700): // snow
+      iconImage.classList.add('wi-snow');
+      document.body.style.backgroundImage = 'linear-gradient(90deg, #e3ffe7 0%, #d9e7ff 100%)';
+      break;
+
+    case (icon < 800): // atmosphere
+      iconImage.classList.add('wi-windy');
+      document.body.style.backgroundImage = 'linear-gradient(90deg, #efd5ff 0%, #515ada 100%)';
+      break;
+
+    case (icon === 800): // clear
+      iconImage.classList.add('wi-day-sunny');
+      document.body.style.backgroundColor = "rgb(0,215,255)";
+      document.body.style.backgroundImage = "linear-gradient(135deg, rgba(0,215,255,1) 0%, rgba(204,247,255,1) 50%, rgba(255,255,255,1) 100%)";
+      break;
+
+    case (icon > 800): // cloud
+      iconImage.classList.add('wi-cloud');
+      document.body.style.backgroundImage = 'linear-gradient(90deg, #3F2B96 0%, #A8C0FF 100%)';
+      break;
+
+    default:
+      alert("none");
+      break;
+  }
+  // location
+  document.getElementById("title").innerHTML = `${apiResponse.name}`;
+  // status
+  document.getElementById("status").innerHTML = `${apiResponse.weather[0].main}`;
+  // temp
+  document.getElementById("temperature").innerHTML = `${Math.round(apiResponse.main.temp)}°F`;
+  // humidity
+  document.getElementById("humidity").innerHTML = `Humidity: ${apiResponse.main.humidity}%`
+  // wind
+  document.getElementById("wind").innerHTML = `Wind: ${Math.round(apiResponse.wind.speed)}mph`
+}
+
+/*
+function renderForecastData(day, apiResponse) {
+  let date = document.querySelector(".day.date");
+  let iconImage = document.querySelector(".day.weather-icon");
+  let high = document.querySelector(".day.forecast-high");
+  let low = document.querySelector(".day.forecast-low");
+  // date
+
+  // status
+  switch (true) {
+
+    case (icon < 300): // thunderstorm
+      iconImage.classList.add('wi-thunderstorm');
+      break;
+
+    case (icon < 400): // drizzle
+      iconImage.classList.add('wi-sprinkle');
+      break;
+
+    case (icon < 600): // rain
+      iconImage.classList.add('wi-rain');
+      break;
+
+    case (icon < 700): // snow
+      iconImage.classList.add('wi-snow');
+      break;
+
+    case (icon < 800): // atmosphere
+      iconImage.classList.add('wi-windy');
+      break;
+
+    case (icon === 800): // clear
+      iconImage.classList.add('wi-day-sunny');
+      break;
+
+    case (icon > 800): // cloud
+      iconImage.classList.add('wi-cloud');
+      break;
+
+    default:
+      break;
+  }
+  // high
+  high.innerHTML = `High: ${apiResponse.main.temp_max}`;
+  // low
+  low.innerHTML = `Low: ${apiResponse.main.temp_min}`;
+}
+      // displays weather icon
+      var iconImage = document.getElementById("weather-icon");
       var icon = response.weather[0].id;
 
       switch (true) {
 
         case (icon < 300): // thunderstorm
-          iconImage.setAttribute("src", "images/005-storm.png");
+          iconImage.classList.add('wi-thunderstorm');
           document.body.style.backgroundColor = 'rgb(30,117,131)';
           document.body.style.backgroundImage = 'linear-gradient(135deg, rgba(30,117,131,1) 0%, rgba(47,73,78,1) 68%, rgba(10,15,15,1) 100%)';
           break;
 
         case (icon < 400): // drizzle
-          iconImage.setAttribute("src", "images/014-drop.png");
+          iconImage.classList.add('wi-sprinkle');
           document.body.style.backgroundImage = 'linear-gradient(90deg, #00d2ff 0%, #3a47d5 100%)';
           break;
 
         case (icon < 600): // rain
-          iconImage.setAttribute("src", "images/007-rain.png");
+          iconImage.classList.add('wi-rain');
           document.body.style.backgroundImage = 'linear-gradient(90deg, #4b6cb7 0%, #182848 100%)';
           break;
 
         case (icon < 700): // snow
-          iconImage.setAttribute("src", "images/002-temperature-1.png");
+          iconImage.classList.add('wi-snow');
           document.body.style.backgroundImage = 'linear-gradient(90deg, #e3ffe7 0%, #d9e7ff 100%)';
           break;
 
         case (icon < 800): // atmosphere
-          iconImage.setAttribute("src", "images/009-wind-1.png");
+          iconImage.classList.add('wi-windy');
           document.body.style.backgroundImage = 'linear-gradient(90deg, #efd5ff 0%, #515ada 100%)';
           break;
 
         case (icon === 800): // clear
-          iconImage.setAttribute("src", "images/016-sun.png");
+          iconImage.classList.add('wi-day-sunny');
           document.body.style.backgroundColor = "rgb(0,215,255)";
           document.body.style.backgroundImage = "linear-gradient(135deg, rgba(0,215,255,1) 0%, rgba(204,247,255,1) 50%, rgba(255,255,255,1) 100%)";
           break;
 
         case (icon > 800): // cloud
-          iconImage.setAttribute("src", "images/015-cloud.png");
+          iconImage.classList.add('wi-cloud');
           document.body.style.backgroundImage = 'linear-gradient(90deg, #3F2B96 0%, #A8C0FF 100%)';
           break;
 
@@ -84,9 +224,23 @@ function getWeather(position) {
       document.getElementById("status").innerHTML = `${response.weather[0].main}`;
       document.getElementById("temperature").innerHTML = `${Math.round(response.main.temp)}°F`;
       document.getElementById("humidity").innerHTML = `Humidity: ${response.main.humidity}%`
-      document.getElementById("wind").innerHTML = `Wind: ${response.wind.speed}mph`
+      document.getElementById("wind").innerHTML = `Wind: ${Math.round(response.wind.speed)}mph`
+      // today high = response.main.temp_max
+      // today low = response.main.temp_min
     }
   }
 }
+*/
 
+// not returning lat + lon as expected
 getLocation();
+
+/*
+let forecast = getForecastData(location.lat, location.lon); // array with 0 as tomorrow, 3 as 4 days
+
+renderForecastData("day-zero", todayWeather);
+renderForecastData(tomorrow, forecast[0]);
+renderForecastData(two days, forecast[1]);
+renderForecastData(three days, forecast[2]);
+renderForecastData(four days, forecast[3]);
+*/
