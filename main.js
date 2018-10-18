2b4698ffe0dc58bea16ce7af5cf2c2639391f290
chase-stevens <chasestevenspersonal@gmail.com>
2018-10-17 18:28:59 -0700

    refactored code

--- main.js
+++ main.js
@@ -54,10 +54,9 @@ function getForecastData(lat, lon) {
   function processRequest(e){
     if (request.readyState === 4 && request.status === 200) {
       var response = JSON.parse(request.responseText);
-      renderForecastData(1, response);
-      renderForecastData(2, response);
-      renderForecastData(3, response);
-      renderForecastData(4, response);
+      for (let i = 1; i < 5; i++) {
+        renderForecastData(i, response);
+      }
     }
   }
 }
